dd72f2b9e682eadd61f754d41a91a3f6880780c7
root <root@ip-10-0-0-94.ap-northeast-1.compute.internal>
2019-06-29 02:33:33 +0000

    文字コードをutf8mb4に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def top
     @systems_title = "Recipes"
     @systems = System.all
-    @blocks_title = "MyCodes"
+    @blocks_title = "Codes"
     @blocks = Block.all
   end
 end
